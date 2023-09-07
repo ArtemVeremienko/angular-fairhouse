a252727c44a6cac3e9b9fb641e6c9cb7197e3c0c
Artem Veremienko <temaspsxxx@gmail.com>
2023-09-07 03:48:21 +0300

    feat: add housing-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HousingListComponent } from './housing-list/housing-list.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HousingListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
